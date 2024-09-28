346340a70ae8bb7635f692161ba2d5f8d1e8b0d9
Zahira-Rejala <rejalazahira@gmail.com>
2024-09-27 22:22:34 -0300

    modulos

--- .py
+++
@@ -1,3 +0,0 @@
-sad
-sadasdawdzxcawdw
-holaolaola
---
+++ modulo_listas.py
@@ -0,0 +1,7 @@
+from UTN_Heroes_Dataset.utn_listas import (
+    lista_alturas_heroes, lista_poder_heroes,
+    lista_identidades_heroes, lista_nombres_heroes,
+    lista_generos_heroes, lista_apodos_heroes
+)
+
+print(lista_alturas_heroes)
